dce3902d98b5b645960c59de710c3b328b2e8f22
Max <max@Andrews-MacBook-Pro.local>
2023-02-25 11:25:40 -0700

    reinitalize the drive subsystem

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,32 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
+  private static Spark frontLeft = new Spark(0);
+  private static Spark rearLeft = new Spark(0);
+  private static Spark frontRight = new Spark(0);
+  private static Spark rearRight = new Spark(0);
+
+  private static MotorControllerGroup rightMotors = new MotorControllerGroup(frontRight, rearRight);
+  private static MotorControllerGroup leftMortors = new MotorControllerGroup(frontLeft, rearLeft);
+
+  private static DifferentialDrive drive = new DifferentialDrive(leftMortors, rightMotors);
+
   public DriveTrain() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public static void Drive(double rightSpeed, double leftSpeed){
+    drive.tankDrive(leftSpeed, rightSpeed);
   }
 }
